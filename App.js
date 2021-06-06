b01dec88bd9c2d30e180b5f9aea44a146a760c06
Heet kumar Kothadiya <78725162+heet-kumar@users.noreply.github.com>
2021-06-06 22:07:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Content from "./Components/Content"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Content />
+      <Content />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
